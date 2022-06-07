fadcb1dcd26dfa91f7af3167bf2972cbed2a951b
ShubhasmitMalakar <55288337+ShubhasmitMalakar@users.noreply.github.com>
2022-06-07 12:15:53 +0530

    copy bug fix

--- components/AppBar.js
+++
@@ -1,9 +0,0 @@
-export default function AppBar() {
-    return (
-        <div>
-            {/* <div style={{ height: "100px", width: "100%", backgroundColor: "pink" }}>
-                AppBar
-            </div> */}
-        </div>
-    )
-}
