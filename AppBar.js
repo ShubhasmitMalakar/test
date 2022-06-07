d956f2a696da5ae8571fdcdde71b1d3fd77f5312
ShubhasmitMalakar <55288337+ShubhasmitMalakar@users.noreply.github.com>
2022-06-07 12:11:12 +0530

    First Commit

---
+++ AppBar.js
@@ -0,0 +1,9 @@
+export default function AppBar() {
+    return (
+        <div>
+            {/* <div style={{ height: "100px", width: "100%", backgroundColor: "pink" }}>
+                AppBar
+            </div> */}
+        </div>
+    )
+}
